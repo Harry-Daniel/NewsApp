dbe9789a983c3b20b6e14944f400690bbde272ed
Harry-Daniel <hayiborkomla@gmail.com>
2024-09-16 23:29:41 +0000

    Fetching Directly from the source

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,8 @@
+import { getAllNews } from "@/components/lib/news";
 import NewsList from "@/components/news-list";
 
-export default async function NewsPage() {
-  const response = await fetch("http://localhost:8080/news");
-  if (!response.ok) {
-    throw new Error("failed to fetch news.");
-  }
-
-  const news = await response.json();
-
+export default function NewsPage() {
+  const news = getAllNews();
   return (
     <section>
       <h1>News Page</h1>
